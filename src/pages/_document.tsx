b18a9fb3413d4ed9517f0955bf995a1d2c32f1fc
minorsetback <vlad232362@gmail.com>
2023-02-21 11:43:57 +0200

    preview

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,25 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  /* eslint-disable @next/next/google-font-display */
+  
   return (
     <Html lang="en">
       <Head />
       <link
-        href="https://fonts.googleapis.com/css?family=Manrope&display=optional"
+        href="https://fonts.googleapis.com/css?family=Manrope&display=block"
         rel="stylesheet"
+        as="style"
       />
       <link
-        href="https://fonts.googleapis.com/css?family=Mulish&display=optional"
+        href="https://fonts.googleapis.com/css?family=Mulish&display=block"
         rel="stylesheet"
+        as="style"
       />
       <link
-        href="https://fonts.googleapis.com/css?family=Poppins&display=optional"
+        href="https://fonts.googleapis.com/css?family=Poppins&display=block"
         rel="stylesheet"
+        as="style"
       />
       <body>
         <Main />
